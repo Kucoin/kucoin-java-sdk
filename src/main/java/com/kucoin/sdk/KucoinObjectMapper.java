fd076276876fa615208e7d83220254918d90c2fb
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-21 01:20:26 +0000

    Test minimally against both the live and sandbox APIs, widen the time gap we accept between local and server

---
+++ src/main/java/com/kucoin/sdk/KucoinObjectMapper.java
@@ -0,0 +1,16 @@
+package com.kucoin.sdk;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class KucoinObjectMapper {
+
+  public static final ObjectMapper INSTANCE;
+
+  static {
+    INSTANCE = new ObjectMapper();
+    INSTANCE.configure(FAIL_ON_UNKNOWN_PROPERTIES, true);
+  }
+
+}
--- src/test/java/com/kucoin/sdk/KucoinRestClientTest.java
+++ src/test/java/com/kucoin/sdk/KucoinRestClientTest.java
@@ -47,10 +47,10 @@ import com.kucoin.sdk.rest.response.WithdrawResponse;
  * Created by chenshiwei on 2019/1/21.
  */
 public class KucoinRestClientTest {
-    private static KucoinRestClient kucoinRestClient;
+    private static KucoinRestClient sandboxKucoinRestClient;
+    private static KucoinRestClient liveKucoinRestClient;
 
     private static Long startAt;
-
     private static Long endAt;
 
     @Rule
@@ -58,8 +58,10 @@ public class KucoinRestClientTest {
 
     @BeforeClass
     public static void setUpClass() {
-        kucoinRestClient = new KucoinClientBuilder().withBaseUrl("https://openapi-sandbox.kucoin.com")
+        sandboxKucoinRestClient = new KucoinClientBuilder().withBaseUrl("https://openapi-sandbox.kucoin.com")
                 .withApiKey("5c42a37bef83c73aa68e43c4", "7df80b16-1b95-4739-9b03-3d987599c332", "asd123456")
+                .buildRestClient();
+        liveKucoinRestClient = new KucoinClientBuilder().withBaseUrl("https://openapi-v2.kucoin.com")
                 .buildRestClient();
         startAt = LocalDateTime.of(2019, 1, 1, 0, 0, 0).toEpochSecond(ZoneOffset.of("+8"));
         endAt = LocalDateTime.of(2019, 1, 21, 0, 0, 0).toEpochSecond(ZoneOffset.of("+8"));
@@ -71,13 +73,13 @@ public class KucoinRestClientTest {
     @Test
     public void accountAPIMulti() throws Exception {
         List<AccountBalancesResponse> accountBalancesResponses
-          = kucoinRestClient.accountAPI().listAccounts(null, null);
+          = sandboxKucoinRestClient.accountAPI().listAccounts(null, null);
         assertThat(accountBalancesResponses.size(), Is.is(6));
     }
 
     @Test
     public void accountAPI() throws Exception {
-        List<AccountBalancesResponse> accountBalancesResponses = kucoinRestClient.accountAPI().listAccounts("BTC", null);
+        List<AccountBalancesResponse> accountBalancesResponses = sandboxKucoinRestClient.accountAPI().listAccounts("BTC", null);
         assertThat(accountBalancesResponses.size(), Is.is(2));
         Optional<AccountBalancesResponse> main = accountBalancesResponses.stream()
                 .filter(accountBalancesResponse -> accountBalancesResponse.getType().equals("main")).findFirst();
@@ -88,28 +90,28 @@ public class KucoinRestClientTest {
 
         String mainAccountId = main.get().getId();
         String tradeAccountId = trade.get().getId();
-        AccountBalanceResponse account = kucoinRestClient.accountAPI().getAccount(mainAccountId);
+        AccountBalanceResponse account = sandboxKucoinRestClient.accountAPI().getAccount(mainAccountId);
         assertThat(account, notNullValue());
 
-        Pagination<AccountDetailResponse> accountHistory = kucoinRestClient.accountAPI().getAccountHistory(mainAccountId,
+        Pagination<AccountDetailResponse> accountHistory = sandboxKucoinRestClient.accountAPI().getAccountHistory(mainAccountId,
                 startAt, endAt, 1, 10);
         assertThat(accountHistory, notNullValue());
 
-        Pagination<AccountHoldsResponse> holds = kucoinRestClient.accountAPI().getHolds(mainAccountId, 1, 10);
+        Pagination<AccountHoldsResponse> holds = sandboxKucoinRestClient.accountAPI().getHolds(mainAccountId, 1, 10);
         assertThat(holds, notNullValue());
 
-        Map<String, String> result = kucoinRestClient.accountAPI().innerTransfer(String.valueOf(System.currentTimeMillis()), tradeAccountId, BigDecimal.valueOf(0.00000001), mainAccountId);
-        kucoinRestClient.accountAPI().innerTransfer(String.valueOf(System.currentTimeMillis()), mainAccountId, BigDecimal.valueOf(0.00000001), tradeAccountId);
+        Map<String, String> result = sandboxKucoinRestClient.accountAPI().innerTransfer(String.valueOf(System.currentTimeMillis()), tradeAccountId, BigDecimal.valueOf(0.00000001), mainAccountId);
+        sandboxKucoinRestClient.accountAPI().innerTransfer(String.valueOf(System.currentTimeMillis()), mainAccountId, BigDecimal.valueOf(0.00000001), tradeAccountId);
         assertThat(result, notNullValue());
 
         exception.expect(KucoinApiException.class);
         exception.expectMessage("account already exists");
-        kucoinRestClient.accountAPI().createAccount("KCS", "main");
+        sandboxKucoinRestClient.accountAPI().createAccount("KCS", "main");
     }
 
     @Test
     public void fillAPI() throws Exception {
-        Pagination<TradeResponse> fills = kucoinRestClient.fillAPI().listFills("ETH-BTC", null, "BUY",
+        Pagination<TradeResponse> fills = sandboxKucoinRestClient.fillAPI().listFills("ETH-BTC", null, "BUY",
                 null, startAt, endAt, 1, 10);
         assertThat(fills, notNullValue());
     }
@@ -119,26 +121,26 @@ public class KucoinRestClientTest {
         OrderCreateApiRequest request = OrderCreateApiRequest.builder()
                 .price(BigDecimal.valueOf(0.000001)).size(BigDecimal.ONE).side("buy")
                 .symbol("ETH-BTC").type("limit").clientOid(UUID.randomUUID().toString()).build();
-        OrderCreateResponse order = kucoinRestClient.orderAPI().createOrder(request);
+        OrderCreateResponse order = sandboxKucoinRestClient.orderAPI().createOrder(request);
         assertThat(order, notNullValue());
 
-        Pagination<OrderResponse> orderResponsePagination = kucoinRestClient.orderAPI().listOrders("ETH-BTC",
+        Pagination<OrderResponse> orderResponsePagination = sandboxKucoinRestClient.orderAPI().listOrders("ETH-BTC",
                 null, null, "active", null, null, 1, 1);
         assertThat(orderResponsePagination, notNullValue());
 
-        OrderResponse orderResponse = kucoinRestClient.orderAPI().getOrder(order.getOrderId());
+        OrderResponse orderResponse = sandboxKucoinRestClient.orderAPI().getOrder(order.getOrderId());
         assertThat(orderResponse, notNullValue());
 
-        OrderCancelResponse orderCancelResponse = kucoinRestClient.orderAPI().cancelOrder(order.getOrderId());
+        OrderCancelResponse orderCancelResponse = sandboxKucoinRestClient.orderAPI().cancelOrder(order.getOrderId());
         assertThat(orderCancelResponse, notNullValue());
 
-        OrderCancelResponse ordersCancelResponse = kucoinRestClient.orderAPI().cancelAllOrders("ETH-BTC");
+        OrderCancelResponse ordersCancelResponse = sandboxKucoinRestClient.orderAPI().cancelAllOrders("ETH-BTC");
         assertThat(ordersCancelResponse, notNullValue());
     }
 
     @Test
     public void withdrawalAPI() throws Exception {
-        Pagination<WithdrawResponse> withdrawList = kucoinRestClient.withdrawalAPI().getWithdrawList("KCS", "FAILURE",
+        Pagination<WithdrawResponse> withdrawList = sandboxKucoinRestClient.withdrawalAPI().getWithdrawList("KCS", "FAILURE",
                 startAt, endAt, 1, 10);
         assertThat(withdrawList, notNullValue());
 
@@ -149,76 +151,92 @@ public class KucoinRestClientTest {
         exception.expectMessage("Sandbox environment cannot be withdrawn");
         WithdrawApplyRequest withdrawApplyRequest = WithdrawApplyRequest.builder().address("123467")
                 .amount(BigDecimal.valueOf(0.00000001)).currency("KCS").build();
-        kucoinRestClient.withdrawalAPI().applyWithdraw(withdrawApplyRequest);
+        sandboxKucoinRestClient.withdrawalAPI().applyWithdraw(withdrawApplyRequest);
 
-        kucoinRestClient.withdrawalAPI().cancelWithdraw("1234567");
+        sandboxKucoinRestClient.withdrawalAPI().cancelWithdraw("1234567");
     }
 
     @Test
     public void depositAPI() throws Exception {
         exception.expect(KucoinApiException.class);
         exception.expectMessage("Sandbox environment cannot get deposit address");
-        kucoinRestClient.depositAPI().createDepositAddress("KCS");
+        sandboxKucoinRestClient.depositAPI().createDepositAddress("KCS");
 
         exception.expect(KucoinApiException.class);
         exception.expectMessage("Sandbox environment cannot get deposit address");
-        kucoinRestClient.depositAPI().getDepositAddress("KCS");
+        sandboxKucoinRestClient.depositAPI().getDepositAddress("KCS");
 
         exception.expect(KucoinApiException.class);
         exception.expectMessage("Sandbox environment cannot get deposit address");
-        kucoinRestClient.depositAPI().getDepositPageList("KCS", startAt, endAt, "SUCCESS", 1, 10);
+        sandboxKucoinRestClient.depositAPI().getDepositPageList("KCS", startAt, endAt, "SUCCESS", 1, 10);
     }
 
     @Test
     public void symbolAPI() throws Exception {
-        TickerResponse ticker = kucoinRestClient.symbolAPI().getTicker("ETH-BTC");
-        assertThat(ticker, notNullValue());
+        assertThat(sandboxKucoinRestClient.symbolAPI().getTicker("ETH-BTC"), notNullValue());
 
-        List<SymbolResponse> symbols = kucoinRestClient.symbolAPI().getSymbols();
+        List<SymbolResponse> symbols = sandboxKucoinRestClient.symbolAPI().getSymbols();
         assertThat(symbols, notNullValue());
         assertThat(symbols.size(), greaterThan(0));
 
-        SymbolTickResponse hrStats = kucoinRestClient.symbolAPI().get24hrStats("ETH-BTC");
+        SymbolTickResponse hrStats = sandboxKucoinRestClient.symbolAPI().get24hrStats("ETH-BTC");
+        assertThat(hrStats, notNullValue());
+    }
+
+    /**
+     * The live and sandbox APIs seem to be divergent. Test against the live API too where
+     * possible
+     */
+    @Test
+    public void symbolAPILive() throws Exception {
+        TickerResponse ticker = liveKucoinRestClient.symbolAPI().getTicker("ETH-BTC");
+        assertThat(ticker, notNullValue());
+
+        List<SymbolResponse> symbols = liveKucoinRestClient.symbolAPI().getSymbols();
+        assertThat(symbols, notNullValue());
+        assertThat(symbols.size(), greaterThan(0));
+
+        SymbolTickResponse hrStats = liveKucoinRestClient.symbolAPI().get24hrStats("ETH-BTC");
         assertThat(hrStats, notNullValue());
     }
 
     @Test
     public void orderBookAPI() throws Exception {
-        OrderBookResponse fullOrderBookAggregated = kucoinRestClient.orderBookAPI().getFullOrderBookAggregated("ETH-BTC");
+        OrderBookResponse fullOrderBookAggregated = sandboxKucoinRestClient.orderBookAPI().getFullOrderBookAggregated("ETH-BTC");
         assertThat(fullOrderBookAggregated, notNullValue());
 
-        OrderBookResponse fullOrderBookAtomic = kucoinRestClient.orderBookAPI().getFullOrderBookAtomic("ETH-BTC");
+        OrderBookResponse fullOrderBookAtomic = sandboxKucoinRestClient.orderBookAPI().getFullOrderBookAtomic("ETH-BTC");
         assertThat(fullOrderBookAtomic, notNullValue());
 
-        OrderBookResponse partOrderBookAggregated = kucoinRestClient.orderBookAPI().getPartOrderBookAggregated("ETH-BTC");
+        OrderBookResponse partOrderBookAggregated = sandboxKucoinRestClient.orderBookAPI().getPartOrderBookAggregated("ETH-BTC");
         assertThat(partOrderBookAggregated, notNullValue());
     }
 
     @Test
     public void historyAPI() throws Exception {
-        List<TradeHistoryResponse> tradeHistories = kucoinRestClient.historyAPI().getTradeHistories("ETH-BTC");
+        List<TradeHistoryResponse> tradeHistories = sandboxKucoinRestClient.historyAPI().getTradeHistories("ETH-BTC");
         assertThat(tradeHistories, notNullValue());
         assertThat(tradeHistories.size(), greaterThan(0));
 
-        List<List<String>> historicRates = kucoinRestClient.historyAPI().getHistoricRates("ETH-BTC", startAt, endAt, "1min");
+        List<List<String>> historicRates = sandboxKucoinRestClient.historyAPI().getHistoricRates("ETH-BTC", startAt, endAt, "1min");
         assertThat(historicRates, notNullValue());
         assertThat(historicRates.size(), greaterThan(0));
     }
 
     @Test
     public void currencyAPI() throws Exception {
-        List<CurrencyResponse> currencies = kucoinRestClient.currencyAPI().getCurrencies();
+        List<CurrencyResponse> currencies = sandboxKucoinRestClient.currencyAPI().getCurrencies();
         assertThat(currencies, notNullValue());
         assertThat(currencies.size(), greaterThan(0));
 
-        CurrencyDetailResponse kcs = kucoinRestClient.currencyAPI().getCurrencyDetail("KCS");
+        CurrencyDetailResponse kcs = sandboxKucoinRestClient.currencyAPI().getCurrencyDetail("KCS");
         assertThat(kcs, notNullValue());
     }
 
     @Test
     public void timeAPI() throws Exception {
-        Long serverTimeStamp = kucoinRestClient.timeAPI().getServerTimeStamp();
-        assertThat(System.currentTimeMillis() - serverTimeStamp, lessThanOrEqualTo(2000L));
+        Long serverTimeStamp = sandboxKucoinRestClient.timeAPI().getServerTimeStamp();
+        assertThat(System.currentTimeMillis() - serverTimeStamp, lessThanOrEqualTo(5000L));
     }
 
 }
