851048617b6bbf1dd8fddb522c8e50800c0103a2
Daz.deng <daz@corp.kucoin.com>
2023-03-03 16:44:49 +0800

    loanAPI add field

--- src/main/java/com/kucoin/sdk/rest/response/BorrowResponse.java
+++ src/main/java/com/kucoin/sdk/rest/response/BorrowResponse.java
@@ -6,10 +6,14 @@ package com.kucoin.sdk.rest.response;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class BorrowResponse {
 
     private String orderId;
 
     private String currency;
+
+    private BigDecimal actualSize;
 }
